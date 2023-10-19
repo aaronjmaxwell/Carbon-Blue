34a72e4e995455327ba185794d2d459db7d9511b
Aaron J. Maxwell, PhD - I only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2023-10-19 10:36:35 -0400

    Beer estimator.

--- Cee/LCTHW/ex1.c
+++ Cee/LCTHW/ex1.c
@@ -1,22 +1,12 @@
 /* Write a simple string to the terminal.
-
-The basic structure of a C program is given here. External definitions
-are included as header files - such as input/output - and the main
-namespace is what is executed first. Function definitions in C require
-a type declaration that matches the return type.
-*/
+The basic structure of a C program is given here.
+External definition are included as header files, such as input/output, and the main namespace is what is executed first. Function definitions in C require a type declaration that matches the return type.*/
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
     /* The default main program.
 
-    Main is a protected namespace in C, and is the point of entry of
-    every script. This function takes two inputs: the number of arguments,
-    and the arguments themselves as a character buffer. It then uses the
-    stdio.puts to 'put a string to the screen'.
-
-	Arguments
-    ---------
+    Main is a protected namespace in C, and is the point of entry of every script. This function takes two inputs: the number of arguments, and the arguments themselves as a character buffer. It then uses the stdio.puts to 'put a string to the screen'. The function takes two arguments
     argc: the number of arguments to main
     argv: the arguments themselves
     */
--- CeePlusPlus/HelloWorld.cpp
+++ CeePlusPlus/HelloWorld.cpp
@@ -1,4 +1,7 @@
-/* Generates a simple Hello World message. */
+/* Generates a simple Hello World message.
+
+As in C, all headers and additional code must be stored here first. Include
+*/
 #include <iostream>
 int main(){
     std::cout << "Hello World!\n";
